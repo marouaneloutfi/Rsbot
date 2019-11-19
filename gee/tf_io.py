64746774e65ad5f902bf58cdc62c9936d7e248be
marouane <marouane.loutfi@gmail.com>
2019-11-19 09:11:11 +0100

    fix tf_io

--- gee/tf_io.py
+++ gee/tf_io.py
@@ -7,14 +7,14 @@ def get_columns(features, kernel_shape):
 
 
 class TfDatasetParser:
-    _bands = ['B2', 'B3', 'B4', 'B5' , 'B6', 'B7', 'B10', 'b11']
+    _bands = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B10', 'B11']
 
     def __init__(self, kernel_size, response):
         self.features = get_selectors(self._bands) + response
         columns = get_columns(self.features, [kernel_size, kernel_size])
         self.feature_dic = dict(zip(self.features, columns))
 
-    def get_dataset(self, tf_dir,kernel_size, shuffle = True):
+    def get_dataset(self, tf_dir,kernel_size, shuffle=True):
         """Get the preprocessed training dataset
       Returns:
         A tf.data.Dataset of training data.
@@ -50,5 +50,15 @@ class TfDatasetParser:
         inputs = tf.stack([stacked[i:i + 8] for i in range(0, 64, 8)], axis=0)
 
         return tf.transpose(inputs, [2, 3, 1, 0]), tf.transpose(outputs, [1, 2, 0])
+
+    def head_dataset(self, dataset, tempo=False):
+        if tempo:
+            example = iter(dataset.take(1)).next()
+            tempo_slices = example[0][0].numpy()[:, :, :len(get_selectors(self._bands))]
+            return [tempo_slices[:, :, i:i + 8] for i in range(0, len(get_selectors(self.bands)), 8)]
+
+
+
+
 
 
