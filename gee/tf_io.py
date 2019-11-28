78342e2592f42baeb2f6e1870aa014c6c7ee4dc5
marouane <marouane.loutfi@gmail.com>
2019-11-28 10:56:19 +0100

    fix for tfDataParser

--- gee/tf_io.py
+++ gee/tf_io.py
@@ -7,12 +7,13 @@ def get_columns(features, kernel_shape):
 
 
 class TfDatasetParser:
-    _bands = ['B2', 'B3', 'B4']
 
-    def __init__(self, kernel_size, response):
-        self.features = get_selectors(self._bands) + response
+    def __init__(self, kernel_size, bands, response, n_bands, n_months):
+        self.features = get_selectors(bands) + response
         columns = get_columns(self.features, [kernel_size, kernel_size])
         self.feature_dic = dict(zip(self.features, columns))
+        self.n_months = n_months
+        self.n_bands = n_bands
 
     def get_dataset(self, tf_dir, n_samples, batch_size, shuffle=True):
         """Get the preprocessed dataset
@@ -46,8 +47,8 @@ class TfDatasetParser:
         """
         inputsList = [inputs.get(key) for key in self.features]
         stacked = tf.stack(inputsList, axis=0)
-        outputs = stacked[64:]
-        inputs = tf.stack([stacked[i:i + 8] for i in range(0, 64, 8)], axis=0)
+        outputs = stacked[self.n_bands*self.n_months:]
+        inputs = tf.stack([stacked[i:i + self.n_bands] for i in range(0, self.n_months*self.n_bands, self.n_bands)], axis=0)
 
         return tf.transpose(inputs, [2, 3, 1, 0]), tf.transpose(outputs, [1, 2, 0])
 
