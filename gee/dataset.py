5fe035245bf75953cb21afd181b45b7e90899849
marouane <marouane.loutfi@gmail.com>
2020-01-21 15:33:41 +0100

    tweaking point_center offset

--- gee/dataset.py
+++ gee/dataset.py
@@ -59,7 +59,7 @@ class Dataset:
             sample = self.data.sample(
                 region=poly,
                 scale=self.scale,
-                numPixels=5,
+                numPixels=1,
                 seed=42,
                 # dropNulls=True
             )
