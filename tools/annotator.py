3991157b5ff8ce463657dbb8f999d6564c9de771
marouane <marouane.loutfi@gmail.com>
2020-01-22 13:44:13 +0100

    draft annotator

--- tools/annotator.py
+++ tools/annotator.py
@@ -1,11 +1,10 @@
-from IPython.display import HTML
+from IPython.display import display, HTML
 
 template = """
 <div id="image" class="image"></div>
-   		<div id="rect"></div>
-    	<div id="bounds"></div>
-        <style>
-        
+<div id="rect"></div>
+<div id="bounds"></div>
+<style>      
         #image {
             width: 385px;
             height: 289px;
@@ -24,60 +23,53 @@ template = """
             display: none;
         }
 
-        </style
+        </style>
 <script type="text/javascript"> 
-			(function () {
-		    var div = document.getElementById('image');
-		    div.addEventListener('mousedown', mousedown);
-		    div.addEventListener('mouseup', mouseup);
-		    div.addEventListener('mousemove', mousemove);
+(function () {
+    var div = document.getElementById('image');
+    div.addEventListener('mousedown', mousedown);
+    div.addEventListener('mouseup', mouseup);
+    div.addEventListener('mousemove', mousemove);
 
-		    var grab = false;
-		    var rect = {
-		        x0: 0,
-		        y0: 0,
-		        x1: 0,
-		        y1: 0
-		    };
+    var grab = false;
+    var rect = {
+    x0: 0,
+    y0: 0,
+    x1: 0,
+    y1: 0
+    };
 
-		    function mousedown(e) {
-		        grab = true;
-		        rect.x0 = e.clientX;
-		        rect.y0 = e.clientY;
+    function mousedown(e) {
+    grab = true;
+    rect.x0 = e.clientX;
+    rect.y0 = e.clientY;
+    }
 
-		    }
+    function mousemove(e) {
+    if (grab) {
+        rect.x1 = e.clientX;
+        rect.y1 = e.clientY;
+        showRect();
+        }
+    }
 
-		    function mousemove(e) {
-		        if (grab) {
-		            rect.x1 = e.clientX;
-		            rect.y1 = e.clientY;
-		            showRect();
-		        }
-		    }
+    function mouseup(e) {
+        grab = false;
+    }
 
-		    function mouseup(e) {
-		        grab = false;
-
-
-		    }
-
-		    function showRect() {
-		        var rectDiv = document.getElementById('rect');	
-		        rectDiv.style.display = 'block';
-		        rectDiv.style.position = 'absolute';
-		        rectDiv.style.left = rect.x0 + 'px';
-		        rectDiv.style.top = rect.y0 + 'px';
-		        rectDiv.style.width = (rect.x1 - rect.x0) + 'px';
-		        rectDiv.style.height = (rect.y1 - rect.y0) + 'px';
-
-		        var boundsDiv = document.getElementById('bounds');
-		        boundsDiv.innerText = 'crop rect: ' + rect.x0 + ',' + rect.y0 + ' to ' + rect.x1 + ',' + rect.y0;
-		    }
-
-			})();
-			
-		</script>
-		%s
+    function showRect() {
+        var rectDiv = document.getElementById('rect');	
+        rectDiv.style.display = 'block';
+        rectDiv.style.position = 'absolute';
+        rectDiv.style.left = rect.x0 + 'px';
+        rectDiv.style.top = rect.y0 + 'px';
+        rectDiv.style.width = (rect.x1 - rect.x0) + 'px';
+        rectDiv.style.height = (rect.y1 - rect.y0) + 'px';
+        var boundsDiv = document.getElementById('bounds');
+        boundsDiv.innerText = 'crop rect: ' + rect.x0 + ',' + rect.y0 + ' to ' + rect.x1 + ',' + rect.y0;
+    }
+    })();
+</script>
 """
 
 
@@ -87,5 +79,5 @@ class Annotator:
         self.folder = folder
         self.parser = folder
 
-    def annotate(self, image):
-        HTML(template % image)
+    def annotate(self):
+        display(HTML(template))
