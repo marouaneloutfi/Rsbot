839d77ce04ffb2c43bdd07711f374cd36e54dd4e
marouane <marouane.loutfi@gmail.com>
2020-02-06 01:23:07 +0100

    update annotator.rs

--- tools/_static/annotator.rs
+++ tools/_static/annotator.rs
@@ -395,7 +395,7 @@ input[type=range]:hover {{
             buffer.ymins += ']';
             buffer.ymaxs += ']';
 
-      google.colab.kernel.invokeFunction('{next}',"''' 12 '''");
+      google.colab.kernel.invokeFunction('{next}',"'''"+ JSON.stringify(buffer) +"'''");
     }}
 
         function skip(){{
